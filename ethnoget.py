6d9fd64b72489156a18101a9656520bd77116493
Nicholas Capsimalis <nicholas.capsimalis@gmail.com>
2018-11-19 09:41:57 -0800

    Combined multiple python codes

--- ethnoget.py
+++ ethnoget.py
@@ -9,9 +9,6 @@ from selenium.webdriver.common.keys import Keys
 import pprint
 from copy import deepcopy
 
-ahi_url="https://my.sa.ucsb.edu/catalog/Current/UndergraduateEducation/AHICourseList.aspx"
-ethno_url='https://my.sa.ucsb.edu/catalog/Current/UndergraduateEducation/EthnicityCourses.aspx'
-
 def p2dict(p_element):
     p_text = p_element.text.strip()
     course_num = p_text.split("-", 1)[0].strip()
@@ -20,10 +17,14 @@ def p2dict(p_element):
     
     return {course_num,course_name}
 
-def getGE(url):
+def ethnoReturn():
+    ethno_url="https://my.sa.ucsb.edu/catalog/Current/UndergraduateEducation/EthnicityCourses.aspx"
+
+
+if __name__!="__main__":
     driver = webdriver.Chrome()
-    driver.get(url)
-    expected_title = "UC Santa Barbara General Catalog - American History and Institutions Course List"
+    driver.get(ethno_url)
+    expected_title = "UC Santa Barbara General Catalog - Ethnicity Requirement Course List"
 
     stripped_title=driver.title.strip()
     if expected_title == stripped_title:
@@ -35,12 +36,16 @@ def getGE(url):
     div_id_content = driver.find_elements_by_xpath("//div[@id='content']")
     div_class_contentpadding = driver.find_elements_by_xpath("//div[@id='content']/div[@class='contentpadding']/p")
     list_of_p_elements = driver.find_elements_by_xpath("//div[@id='content']/div[@class='contentpadding']/p")
-    AHI_dict = dict(list(map(p2dict,list_of_p_elements)))
+##    print(div_id_content)
+##    print(div_class_contentpadding)
+##    print(list_of_p_elements)
+
+    ethno_dict = dict(list(map(p2dict,list_of_p_elements)))
+
+##        pprint.pprint(ethno_dict)
     driver.close()
-    return AHI_dict
+    
+##if __name__!="__main__":
+    return ethno_dict
 
-if __name__=="__main__":
-    mydict = getGE(ahi_url)
-    other_dict = getGE(ethno_url)
-
-
+ethnoReturn()
---
+++ master.py
@@ -0,0 +1,11 @@
+import AHIget
+##import ethnoget
+import pprint
+
+ethno_dict = AHIget.getGE(AHIget.ethno_url)
+
+pprint.pprint(ethno_dict)
+##ethno_dict = ethnoget.ethnoReturn()
+
+
+####pprint.pprint(ethno_dict)
