204872b05095a1cad0bb295afc1c02c9c110c97d
Nicholas Capsimalis <nicholas.capsimalis@gmail.com>
2018-11-22 12:20:32 -0800

    Added HTML framework

--- master.py
+++ master.py
@@ -1,11 +1,32 @@
+import os
+
 import AHIget
-##import ethnoget
 import pprint
 
-ethno_dict = AHIget.getGE(AHIget.ethno_url)
+from flask import Flask, url_for, render_template, request
+from flask import redirect
+from flask import session
 
-pprint.pprint(ethno_dict)
-##ethno_dict = ethnoget.ethnoReturn()
+app = Flask(__name__)
+
+app.secret_key='l10odlkashb1097';
+
+@app.route('/')
+def renderHome():
+    return render_template('home.html')
+
+@app.route('/results', methods=['GET', 'POST'])
+def renderResults():
+    session['requirements']='No'
+    if request.form['ethno']=='ethno': 
+##        session['requirements'].append('Yes')
+        session['requirements']='Yes'
+    return render_template('results.html')
 
 
-####pprint.pprint(ethno_dict)
+##def ethnoGet():
+##    ethno_dict = AHIget.getGE(AHIget.ethno_url)
+##    pprint.pprint(ethno_dict)
+
+if __name__ == "__main__":
+    app.run(debug=True,host="0.0.0.0",port=5000)
